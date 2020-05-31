5d452b4a7234c704bc6065b875c08df36747ffb3
Dariusz Ruszkiewicz <dariusz.ruszkiewicz@bader-leather.com>
2020-05-31 20:22:17 +0200

    some adjustment

--- index.js
+++ index.js
@@ -7,5 +7,11 @@ const result = document.querySelector('.showResult-JS');
 const checkButton = document.querySelector('.checkButton-JS').addEventListener('click', () => {
    const inputValue = document.querySelector('#inputValue-JS').value;
    //inputValue = 5193080150954111;
-   result.innerHTML = getCardProvider(inputValue);
+   //371449635398431
+   //6011000990139424
+   try {
+      result.innerHTML = getCardProvider(inputValue);
+   } catch (error) {
+      result.innerHTML = error;
+   }
 });
