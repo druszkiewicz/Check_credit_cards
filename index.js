b38d7e33609a55eef96077663308fdaba4407eb6
Dariusz Ruszkiewicz <dariusz.ruszkiewicz@bader-leather.com>
2020-05-31 20:12:08 +0200

    działa po uruchomieniu parcel index.html

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import { getCardProvider } from './getCardProvider';
 
 //const { getCardProvider } = require('../getCardProvider');
+
+const result = document.querySelector('.showResult-JS');
+
+const checkButton = document.querySelector('.checkButton-JS').addEventListener('click', () => {
+   const inputValue = document.querySelector('#inputValue-JS').value;
+   //inputValue = 5193080150954111;
+   result.innerHTML = getCardProvider(inputValue);
+});
