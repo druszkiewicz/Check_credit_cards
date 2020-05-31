fb4c594f93834b6ef4536ffcbee198e6f37f886f
Dariusz Ruszkiewicz <dariusz.ruszkiewicz@bader-leather.com>
2020-05-31 18:10:24 +0200

    some changes but modules still don't work

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 import { getCardProvider } from './getCardProvider';
 
 //const { getCardProvider } = require('../getCardProvider');
-
-//const result = getCardProvider(5193080150954111);
-// const result = getCardProvider(6011000990139424);
-// console.log(result);
